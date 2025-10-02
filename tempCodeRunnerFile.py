5ee66e939bd14eeafe3874a833f3921d3daa893e
amirex1835 <amir1385bahrami1@gmail.com>
2025-10-02 12:42:58 +0330

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-# خواندن فایل قبلی
-    if os.path.exists(SAVE_FILE):
-        with open(SAVE_FILE, "r", encoding="utf-8") as f:
-            saved_data = json.load(f)
-    else:
-        saved_data = {}
